89adc9f6cd883b38d5cc58915e798eab32f32588
tricarte <sonagirbas@gmail.com>
2017-04-16 20:12:21 +0300

    subdirectory code commented and 404 handler added

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ require_once __DIR__ . '/vendor/autoload.php';
 $base  = dirname($_SERVER['PHP_SELF']);
 
 // Update request when we have a subdirectory
-if(ltrim($base, '/')) {
-    $_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], strlen($base));
-}
+// if(ltrim($base, '/')) {
+//     $_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], strlen($base));
+// }
 
 // Dispatch as always
 $klein = new \Klein\Klein();
@@ -16,6 +16,10 @@ $klein->respond('GET', '/', function ($req, $res) {
         'a' => 'b',
     ];
     $res->json($x);
+});
+
+$klein->onHttpError(function ($code, $router, $matched) {
+    $router->response()->body('Error: ' . $code);
 });
 
 $klein->dispatch();
