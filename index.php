c9669b8f7e5bc5c355838abac28b97f51eebf619
tricarte <sonagirbas@gmail.com>
2017-04-16 20:14:35 +0300

    Small edit

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-$base  = dirname($_SERVER['PHP_SELF']);
-
 // Update request when we have a subdirectory
+// $base  = dirname($_SERVER['PHP_SELF']);
 // if(ltrim($base, '/')) {
 //     $_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], strlen($base));
 // }
