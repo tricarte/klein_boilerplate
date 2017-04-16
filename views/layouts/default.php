adaaa82f1a52ea9d2263281c0bf7f68c44ed706d
tricarte <sonagirbas@gmail.com>
2017-04-16 23:14:42 +0300

    jquery and bootstrap installable using bower

--- views/layouts/default.php
+++ views/layouts/default.php
@@ -7,6 +7,7 @@
   <meta name="description" content="Site description">
   <meta name="author" content="Author">
 
+  <link rel="stylesheet" href="static/components/boostrap/dist/bootstrap.css">
   <link rel="stylesheet" href="static/css/styles.css?v=1.0">
 
   <!--[if lt IE 9]>
@@ -16,6 +17,8 @@
 
 <body>
   <?= $this->yieldView(); ?>
+  <script src="static/components/jquery/dist/jquery.js"></script>
+  <script src="static/components/bootstrap/dist/js/bootstrap.js"></script>
   <script src="static/js/scripts.js"></script>
 </body>
 </html>
