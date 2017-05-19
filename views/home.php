caa6c5e6ab24846f81e899c445d01c87806781e7
tricarte <sonagirbas@gmail.com>
2017-05-19 23:39:24 +0300

    Merge fixes

--- views/home.php
+++ views/home.php
@@ -1,6 +1,2 @@
 <p>This content comes from 'views/home.php' rendered inside 'views/layouts/default.php'.</p>
-<<<<<<< HEAD
 <p><a href="/about">Here is another view</a></p>
-=======
-<p><a href="/api">Here is another view</a></p>
->>>>>>> 232452d206e5fb4763c71dd269b987f3bbb93b5f
